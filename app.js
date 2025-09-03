f90c0c4049540ec56fcae15b9b2767bc686c8837
Thallyta123 <thallytamayara01@gmail.com>
2025-09-03 23:20:50 +0000

    add.app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 var express = require('express')
-, cookieparsser= require('cookie-parser')
-, session = require('express-session')
-, routes = require('./routes');
-var app  = express();
+  , cookieparsser = require('cookie-parser')
+  , session = require('express-session')
+  , metodoverritede = require('method-override')
+  , load = require('express-load')
+var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(cookieparsser('ntalk'));
 app.use(session());
 app.use(express.urlencoded());
-app.use(express.method0verride());
+app.use(metodoverritede());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-app.get('/', routes);
-//app.get('/usuarios', routes.users.index);
 
-app.listen(3000, function(){
+load('models').then('controllers').then('routes').into(app)
+
+
+app.listen(3000, function () {
   console.log("Ntalk no ar.");
 });
 
