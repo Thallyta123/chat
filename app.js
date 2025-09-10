c971395230f81822824a24ed90abd0b7bbbbea55
Thallyta123 <thallytamayara01@gmail.com>
2025-09-10 23:25:09 +0000

    add.app.js

--- app.js
+++ app.js
@@ -5,19 +5,28 @@ var express = require('express')
   , load = require('express-load')
 var app = express();
 
-app.set('views', __dirname + '/views');
+app.use(express.json());
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 app.use(cookieparsser('ntalk'));
 app.use(session());
 app.use(express.urlencoded());
 app.use(metodoverritede());
-app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+app.use(app.routes);
 app.use(express.static(__dirname + '/public'));
 
 load('models').then('controllers').then('routes').into(app)
 
-
-app.listen(3000, function () {
+app.listen(3001, function () {
   console.log("Ntalk no ar.");
 });
 
+app.use(function(req, res, next) {
+  res.status(404);
+  res.render('not-found');
+});
+app.use(function(error, req, res, next) {
+  res.status(500);
+  res.render('server-error', error);
+});
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+module.exports = function (app) {
+  /* var home = app.controllers.home;
+  app.get('/', home.index);
+  app.post('/entrar', home.login)
+  app.get('/sair', home.logout); */
+};
